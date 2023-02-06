42adb2ded45bee54a44b3db16fa1423247d8db7d
le10004909 <124591192+le10004909@users.noreply.github.com>
2023-02-06 22:14:10 +0800

    Update diy-part1.sh

--- devices/x86_64/diy-part1.sh
+++ devices/x86_64/diy-part1.sh
@@ -16,6 +16,5 @@
 # Add a feed source
 
 cat >> feeds.conf.default <<EOF
-src-git kenzo https://github.com/kenzok8/openwrt-packages
-src-git passwall https://github.com/xiaorouji/openwrt-passwall
+src-git kenzo https://github.com/le10004909/small-package
 EOF
