e12471c5947974d23f797a89c57485a294a473a3
le10004909 <124591192+le10004909@users.noreply.github.com>
2023-02-06 22:14:33 +0800

    Update diy-part2.sh

--- devices/x86_64/diy-part2.sh
+++ devices/x86_64/diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.81.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 # rm -rf package/lean/luci-theme-argon
@@ -19,8 +19,3 @@ find ./ -name luci-theme-argon | xargs rm -rf;
 find ./ -name luci-app-argon-config | xargs rm -rf;
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
-
-# Add Packages
-git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-git clone https://github.com/MapesxGM/luci-app-tencentddns package/luci-app-tencentddns
-git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
